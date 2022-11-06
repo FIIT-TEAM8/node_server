f3d04307537594da7abf6af5a0e4ac29ad9e41e1
ykb <jakubhlavacka123@gmail.com>
2022-11-06 08:21:03 +0100

    added mocha to eslint config

--- test/test.js
+++ test/test.js
@@ -5,12 +5,14 @@ const server = require("../index");
 chai.should();
 chai.use(chaiHttp);
 
+// source: https://www.digitalocean.com/community/tutorials/test-a-node-restful-api-with-mocha-and-chai
 describe("/api/", () => {
-  it("response with ok equals true, blank data and msg", () => {
+  it("response with ok equals true, blank data and msg", (done) => {
     chai.request(server)
       .get("/api/")
       .end((err, res) => {
         res.should.have.status(200);
+        done();
       });
   });
 });
