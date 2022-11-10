965f90c069dbafa6b1599cd1e5b95016762864e5
ykb <jakubhlavacka123@gmail.com>
2022-11-10 15:14:15 +0100

    tests on data routes

--- test/data_routes.test.js
+++ test/data_routes.test.js
@@ -10,22 +10,28 @@ chai.should();
 chai.use(chaiHttp);
 
 describe("/api/data/search", () => {
+  afterEach(() => {
+    sandBox.restore();
+  });
+
   it("valid response with fake data", (done) => {
+    const fakeArticleData = {
+      _id: "627341fb4ec0a74b7ab83632",
+      keywords: ["test"],
+      language: "en",
+      link: ["https://www.test.com/test"],
+      preview: "test preview",
+      published: ["Wed, 04 May 2022 10:27:58 GMT"],
+      region: "gb",
+      title: ["Test"],
+    };
+
     const responseObject = {
       json: () => ({
         page_num: 1,
         per_page: 10,
         query: "test example",
-        results: [{
-          _id: "627341fb4ec0a74b7ab83632",
-          keywords: ["test"],
-          language: "en",
-          link: ["https://www.test.com/test"],
-          preview: "test preview",
-          published: ["Wed, 04 May 2022 10:27:58 GMT"],
-          region: "gb",
-          title: ["Test"],
-        }],
+        results: [fakeArticleData],
         search_from: "",
         search_to: "",
         total_pages: 1,
@@ -49,6 +55,9 @@ describe("/api/data/search", () => {
         res.body.should.have.property("data").have.property("search_to").eql("");
         res.body.should.have.property("data").have.property("total_pages").eql(1);
         res.body.should.have.property("data").have.property("total_results").eql(1);
+        res.body.should.have.property("data").have.property("results").to.be.an("array")
+          .should.contain.an("object");
+        res.body.should.have.nested.property("data.results[0]").eql(fakeArticleData);
         done();
       });
   });
@@ -67,8 +76,59 @@ describe("/api/data/search", () => {
         done();
       });
   });
+});
 
+describe("/api/data/report", () => {
   afterEach(() => {
     sandBox.restore();
+  });
+
+  it("valid response with data", (done) => {
+    const fakeArticleData = {
+      _id: "6239b1eddf4b7decb33fbaf2",
+      html: "scraped html",
+      keywords: ["Assassination"],
+      language: "en",
+      link: "https://www.test.com/test",
+      published: "2020-01-01",
+      region: "gb",
+      title: "Test article",
+    };
+
+    const responseObject = {
+      json: () => ({
+        results: [
+          fakeArticleData,
+        ],
+      }),
+    };
+
+    sandBox.stub(fetch, "Promise").returns(Promise.resolve(responseObject));
+
+    chai.request(server)
+      .get("/api/data/search?ids=[6239b1eddf4b7decb33fbaf2]")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("ok").eql(true);
+        res.body.should.have.property("data").have.property("results");
+        res.body.should.have.property("data").have.property("results").to.be.an("array")
+          .should.contain.an("object");
+        res.body.should.have.nested.property("data.results[0]").eql(fakeArticleData);
+        done();
+      });
+  });
+
+  it("fail fetch, throw exception", (done) => {
+    const error = new Error("Test error");
+    sandBox.stub(fetch, "Promise").throws(error);
+
+    chai.request(server)
+      .get("/api/data/report?ids=[fail]")
+      .end((err, res) => {
+        res.should.have.status(500);
+        res.body.should.have.property("ok").eql(false);
+        res.body.should.have.property("msg").eql("Forwarding request to /report failed.");
+        done();
+      });
   });
 });
