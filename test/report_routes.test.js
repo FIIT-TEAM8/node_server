e64db8e763cb76a784c52a21b68655a590d2513c
ykb <jakubhlavacka123@gmail.com>
2022-11-11 20:14:00 +0100

    get report route tests

--- test/report_routes.test.js
+++ test/report_routes.test.js
@@ -1,68 +1,81 @@
-// // setup + inspiration for should assertions: https://stackoverflow.com/questions/46575524/chai-to-test-json-api-output
+// setup + inspiration for should assertions: https://stackoverflow.com/questions/46575524/chai-to-test-json-api-output
 
-// const sandBox = require("sinon").createSandbox();
-// const chai = require("chai");
-// const chaiHttp = require("chai-http");
-// const jwt = require("jsonwebtoken");
-// const db = require("../db/postgres");
-// const server = require("../index");
+const sandBox = require("sinon").createSandbox();
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const jwt = require("jsonwebtoken");
+const db = require("../db/postgres");
+const server = require("../index");
 
-// chai.should();
-// chai.use(chaiHttp);
+chai.should();
+chai.use(chaiHttp);
 
-// const error = new Error("");
+const error = new Error("test error");
 
-// describe("/api/report", () => {
-//   let mockDb;
+describe("/api/report", () => {
+  let mockDb;
+  let mockJWT;
 
-//   beforeEach(() => {
-//     sandBox.restore();
-//     mockDb = sandBox.mock(db);
-//   });
+  const fakeAuthenticatedData = {
+    username: "test123",
+    iat: "test-iat123",
+    id: 1,
+  };
 
-//   it("fail getting report from db", async () => {
-//     const jwtVerifyStub = sandBox.stub(jwt, "verify");
-//     jwtVerifyStub.returns({
-//       username: "test123",
-//       iat: "test-iat123",
-//       id: 1,
-//     });
-//     mockDb.expects("query").once().resolves({ rows: [] });
+  const fakeArticlesInReport = ["6240f3e3fcf239665d512756", "62709aceabf5bf8856f1a62f"];
 
-//     const res = await chai.request(server)
-//       .get("/api/report/1?status=In+Progress")
-//       .set("Cookie", "__authToken=test123");
+  beforeEach(() => {
+    sandBox.restore();
+    mockDb = sandBox.mock(db);
+    mockJWT = sandBox.mock(jwt);
+  });
 
-//     res.should.have.status(400);
-//     res.body.should.have.property("ok").eql(false);
-//     res.body.should.have.property("msg").eql("Unable to retrieve report.");
-//     mockDb.verify();
-//   });
+  it("fail getting report from db", async () => {
+    mockDb.expects("query").once().resolves({ rows: [] });
+    mockJWT.expects("verify").once().returns({
+      username: "test123",
+      iat: "test-iat123",
+      id: 1,
+    });
 
-//   it("fail getting report, throw execption", async () => {
-//     const jwtVerifyStub = sandBox.stub(jwt, "verify");
-//     jwtVerifyStub.returns({
-//       username: "test123",
-//       iat: "test-iat123",
-//       id: 1,
-//     });
-//     mockDb.expects("query").once().throws(error);
+    const res = await chai.request(server)
+      .get("/api/report/1?status=In+Progress")
+      .set("Cookie", "__authToken=test123");
 
-//     const res = await chai.request(server)
-//       .get("/api/report/1")
-//       .set("Cookie", "__authToken=test123");
+    res.should.have.status(400);
+    res.body.should.have.property("ok").eql(false);
+    res.body.should.have.property("msg").eql("Unable to retrieve report.");
+    sandBox.verify();
+  });
 
-//     res.should.have.status(500);
-//     res.body.should.have.property("ok").eql(false);
-//     res.body.should.have.property("msg").eql("Unable to retrieve report.");
-//     mockDb.verify();
-//   });
+  it("fail getting report, throw execption", async () => {
+    mockJWT.expects("verify").once().returns(fakeAuthenticatedData);
+    mockDb.expects("query").once().throws(error);
 
-//   // it("successfully returned report data", async () => {
-//   //   const res = await chai.request(server)
-//   //     .get("/api/report/1?status=In+Progress")
-//   //     .set("Cookie", "__authToken=test123");
+    const res = await chai.request(server)
+      .get("/api/report/1")
+      .set("Cookie", "__authToken=test123");
 
-//   //   res.should.have.status(200);
-//   // });
-// });
+    res.should.have.status(500);
+    res.body.should.have.property("ok").eql(false);
+    res.body.should.have.property("msg").eql("Unable to retrieve report.");
+    sandBox.verify();
+  });
+
+  it("successfully returned report data", async () => {
+    mockJWT.expects("verify").once().returns(fakeAuthenticatedData);
+    mockDb.expects("query").once().resolves({
+      rows: [{ id: 1, content: fakeArticlesInReport }],
+    });
+
+    const res = await chai.request(server)
+      .get("/api/report/1?status=In+Progress")
+      .set("Cookie", "__authToken=test123");
+
+    res.should.have.status(200);
+    res.body.should.have.property("ok").eql(true);
+    res.body.should.have.property("msg").eql("Report was successfully retrieved.");
+    res.body.should.have.property("articlesInReport").eql(fakeArticlesInReport);
+    sandBox.verify();
+  });
+});
