6245637ff666a9cda05d86f5f6ae438dd6ab6d07
ykb <jakubhlavacka123@gmail.com>
2022-11-13 09:29:46 +0100

    refactor data routes tests

--- test/data_routes.test.js
+++ test/data_routes.test.js
@@ -10,11 +10,17 @@ chai.should();
 chai.use(chaiHttp);
 
 describe("/api/data/search", () => {
+  let fetchStub;
+
+  beforeEach(() => {
+    fetchStub = sandBox.stub(fetch, "Promise");
+  });
+
   afterEach(() => {
     sandBox.restore();
   });
 
-  it("valid response with fake data", (done) => {
+  it("valid response with fake data", async () => {
     const fakeArticleData = {
       _id: "627341fb4ec0a74b7ab83632",
       keywords: ["test"],
@@ -40,50 +46,56 @@ describe("/api/data/search", () => {
     };
 
     // source: https://stackoverflow.com/questions/43960646/testing-mocking-node-fetch-dependency-that-it-is-used-in-a-class-method
-    sandBox.stub(fetch, "Promise").returns(Promise.resolve(responseObject));
+    fetchStub.returns(Promise.resolve(responseObject));
 
-    chai.request(server)
-      .get("/api/data/search?q=test+example")
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.have.property("ok").eql(true);
-        res.body.should.have.property("data").have.property("page_num").eql(1);
-        res.body.should.have.property("data").have.property("per_page").eql(10);
-        res.body.should.have.property("data").have.property("query").eql("test example");
-        res.body.should.have.property("data").have.property("results");
-        res.body.should.have.property("data").have.property("search_from").eql("");
-        res.body.should.have.property("data").have.property("search_to").eql("");
-        res.body.should.have.property("data").have.property("total_pages").eql(1);
-        res.body.should.have.property("data").have.property("total_results").eql(1);
-        res.body.should.have.property("data").have.property("results").to.be.an("array")
-          .should.contain.an("object");
-        res.body.should.have.nested.property("data.results[0]").eql(fakeArticleData);
-        done();
-      });
+    const res = await chai.request(server)
+      .get("/api/data/search?q=test+example");
+
+    res.should.have.status(200);
+    res.body.should.have.property("ok").eql(true);
+    res.body.should.have.property("data").have.property("page_num").eql(1);
+    res.body.should.have.property("data").have.property("per_page").eql(10);
+    res.body.should.have.property("data").have.property("query").eql("test example");
+    res.body.should.have.property("data").have.property("results");
+    res.body.should.have.property("data").have.property("search_from").eql("");
+    res.body.should.have.property("data").have.property("search_to").eql("");
+    res.body.should.have.property("data").have.property("total_pages").eql(1);
+    res.body.should.have.property("data").have.property("total_results").eql(1);
+    res.body.should.have.property("data").have.property("results").to.be.an("array")
+      .should.contain.an("object");
+    res.body.should.have.nested.property("data.results[0]").eql(fakeArticleData);
+
+    sandBox.assert.calledOnce(fetchStub);
   });
 
-  it("fail fetch and throw error", (done) => {
-    // source https://stackoverflow.com/questions/39387822/how-to-handle-sinon-stub-throws-in-unit-test-by-sinon-js
+  it("fail fetch and throw error", async () => {
     const error = new Error("Test error");
-    sandBox.stub(fetch, "Promise").throws(error);
+    // source https://stackoverflow.com/questions/39387822/how-to-handle-sinon-stub-throws-in-unit-test-by-sinon-js
+    fetchStub.throws(error);
 
-    chai.request(server)
-      .get("/api/data/search?q=test+example")
-      .end((err, res) => {
-        res.should.have.status(500);
-        res.body.should.have.property("ok").eql(false);
-        res.body.should.have.property("msg").eql("Forwarding request to /search failed.");
-        done();
-      });
+    const res = await chai.request(server)
+      .get("/api/data/search?q=test+example");
+
+    res.should.have.status(500);
+    res.body.should.have.property("ok").eql(false);
+    res.body.should.have.property("msg").eql("Forwarding request to /search failed.");
+
+    sandBox.assert.calledOnce(fetchStub);
   });
 });
 
 describe("/api/data/report", () => {
+  let fetchStub;
+
+  beforeEach(() => {
+    fetchStub = sandBox.stub(fetch, "Promise");
+  });
+
   afterEach(() => {
     sandBox.restore();
   });
 
-  it("valid response with data", (done) => {
+  it("valid response with data", async () => {
     const fakeArticleData = {
       _id: "6239b1eddf4b7decb33fbaf2",
       html: "scraped html",
@@ -103,32 +115,32 @@ describe("/api/data/report", () => {
       }),
     };
 
-    sandBox.stub(fetch, "Promise").returns(Promise.resolve(responseObject));
+    fetchStub.returns(Promise.resolve(responseObject));
 
-    chai.request(server)
-      .get("/api/data/search?ids=[6239b1eddf4b7decb33fbaf2]")
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.have.property("ok").eql(true);
-        res.body.should.have.property("data").have.property("results");
-        res.body.should.have.property("data").have.property("results").to.be.an("array")
-          .should.contain.an("object");
-        res.body.should.have.nested.property("data.results[0]").eql(fakeArticleData);
-        done();
-      });
+    const res = await chai.request(server)
+      .get("/api/data/search?ids=[6239b1eddf4b7decb33fbaf2]");
+
+    res.should.have.status(200);
+    res.body.should.have.property("ok").eql(true);
+    res.body.should.have.property("data").have.property("results");
+    res.body.should.have.property("data").have.property("results").to.be.an("array")
+      .should.contain.an("object");
+    res.body.should.have.nested.property("data.results[0]").eql(fakeArticleData);
+
+    sandBox.assert.calledOnce(fetchStub);
   });
 
-  it("fail fetch, throw exception", (done) => {
+  it("fail fetch, throw exception", async () => {
     const error = new Error("Test error");
-    sandBox.stub(fetch, "Promise").throws(error);
+    fetchStub.throws(error);
 
-    chai.request(server)
-      .get("/api/data/report?ids=[fail]")
-      .end((err, res) => {
-        res.should.have.status(500);
-        res.body.should.have.property("ok").eql(false);
-        res.body.should.have.property("msg").eql("Forwarding request to /report failed.");
-        done();
-      });
+    const res = await chai.request(server)
+      .get("/api/data/report?ids=[fail]");
+
+    res.should.have.status(500);
+    res.body.should.have.property("ok").eql(false);
+    res.body.should.have.property("msg").eql("Forwarding request to /report failed.");
+
+    sandBox.assert.calledOnce(fetchStub);
   });
 });
